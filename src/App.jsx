b80720eabb62ab04cd7c3e409987540f2dc43eb7
Colson <colsoncalifornication@gmail.com>
2023-06-25 23:09:21 -0400

    pick a random photo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+function getRandomPhoto() {
+  const photos = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+  return photos[Math.floor(Math.random() * photos.length)]
+}
 
 function App() {
-  return <h1>photogram</h1>;
+  const [photos, setPhotos] = useState([]);
+
+  const handleClick = () => {
+    setPhotos([...photos, getRandomPhoto()])
+  };
+
+  return (
+    <div>
+      <h1>photogram</h1>
+      <button onClick={handleClick}>Add Photo</button>
+      <div>
+        <h1>{photos}</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
